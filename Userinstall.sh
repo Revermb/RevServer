f95c2df4a92b0e74ca8855b033f76d0e6aa5923e
Revermb <remi.vermeer@outlook.com>
2025-06-01 03:14:54 +0200

    improved qbittorrent

--- Userinstall.sh
+++ Userinstall.sh
@@ -8,7 +8,7 @@
 
 # Update and install packages
 apt update && apt upgrade -y
-apt install xrdp qbittorrent-nox docker wget -y
+apt install xrdp qbittorrent-nox wget -y
 
 #Add users
 adduser minecraft --quiet --system --group
@@ -33,22 +33,41 @@ mkdir -p /home/qbituser
 wget -O /home/minecraft/server.jar https://piston-data.mojang.com/v1/objects/e6ec2f64e6080b9b5d9b471b291c33cc7f509733/server.jar
 
 # Make sure the minecraft user owns the directory & Jar
-echo \n/////////////////////\npermissions for minecraft setup
+echo \n/////////////////////\n permissions for minecraft setup
 chown -R minecraft:minecraft /home/minecraft
 chmod 766 /home/minecraft/server.jar
 
 #Setup Minecraft server
-echo \n/////////////////////\nsetting up minecraft
+echo \n/////////////////////\n setting up minecraft
 cd /home/minecraft
 java -jar /home/minecraft/server.jar --nogui
 
 #Setup Qbittorrent-nox server
-echo \n/////////////////////\nsetting up qbittorrent
+echo \n/////////////////////\n setting up qbittorrent
 cd /home/qbituser
-su qbituser
-echo exit webinterface with ctrl + c
-qbittorrent-nox
-exit
+
+# Create qBittorrent service configuration directory
+mkdir -p /home/qbituser/.config/qBittorrent
+chown -R qbituser:qbituser /home/qbituser/.config/qBittorrent
+
+# Set proper permissions for qBittorrent home
+chmod 750 /home/qbituser
+
+# Create default qBittorrent config (optional but recommended)
+cat <<EOF > /home/qbituser/.config/qBittorrent/qBittorrent.conf
+[LegalNotice]
+Accepted=true
+
+[Preferences]
+WebUI\Username=admin
+WebUI\Port=8080
+WebUI\CSRFProtection=true
+WebUI\ClickjackingProtection=true
+WebUI\SecureCookie=true
+EOF
+
+chown qbituser:qbituser /home/qbituser/.config/qBittorrent/qBittorrent.conf
+chmod 600 /home/qbituser/.config/qBittorrent/qBittorrent.conf
 
 # Create systemd service file for Minecraft
 echo \n/////////////////////\nCreating Systemd services
