0990a03aa0ca4e5f841d43da27e737746df8812a
Revermb <remi.vermeer@outlook.com>
2025-06-01 03:04:10 +0200

    pain

--- Userinstall.sh
+++ Userinstall.sh
@@ -15,10 +15,10 @@ adduser minecraft --quiet --system --group
 echo "Create password for minecraft user"
 passwd minecraft
 sudo usermod -s /usr/sbin/nologin minecraft
-adduser qbittorrent --quiet --system --group
+adduser qbituser --quiet --system --group
 echo "Create password for qbittorrent user"
-passwd qbittorrent
-sudo usermod -s /usr/sbin/nologin qbittorrent
+passwd qbituser
+sudo usermod -s /usr/sbin/nologin qbituser
 
 # Install Java (globally)
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
@@ -27,7 +27,7 @@ apt-get install -f -y
 
 # Create the directories if they don't exist
 mkdir -p /home/minecraft
-mkdir -p /home/qbittorrent
+mkdir -p /home/qbituser
 
 # Download the Minecraft server JAR
 wget -O /home/minecraft/server.jar https://piston-data.mojang.com/v1/objects/e6ec2f64e6080b9b5d9b471b291c33cc7f509733/server.jar
@@ -44,10 +44,11 @@ java -jar /home/minecraft/server.jar --nogui
 
 #Setup Qbittorrent-nox server
 echo \n/////////////////////\nsetting up qbittorrent
-cd /home/qbittorrent
-su qbittorrent
+cd /home/qbituser
+su qbituser
 echo exit webinterface with ctrl + c
 qbittorrent-nox
+exit
 
 # Create systemd service file for Minecraft
 echo \n/////////////////////\nCreating Systemd services
@@ -68,7 +69,7 @@ EOF
 
 # Reload systemd, enable and start services
 systemctl daemon-reload
-systemctl enable qbittorrent-nox@qbittorrent
+systemctl enable qbittorrent-nox@qbituser
 systemctl enable minecraft.service
-systemctl start qbittorrent-nox@qbittorrent
+systemctl start qbittorrent-nox@qbituser
 systemctl start minecraft.service
